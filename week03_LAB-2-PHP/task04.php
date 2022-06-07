bb9cdb533ef120857fc2d1d29a2ee4b112df6016
Moshiur Rahman <sowrav106@gmail.com>
2022-06-07 22:55:38 +0600

    task 04 is fully replaced

--- week03_LAB-2-PHP/task04.php
+++ week03_LAB-2-PHP/task04.php
@@ -1,21 +1,17 @@
 <?php
+$x=567; $y=43; $z=1332;
 
-	$x = 45; $y=70, $z=80;
-
-
+  $largest = $x;
 
   if ($x >= $y && $x >= $z)
     $largest = $x;
-  
   if ($y >= $x && $y >= $z)
     $largest = $y;
-  
   if ($z >= $x && $z >= $y)
     $largest = $z;
+
+echo "Largest number from three numbers $x, $y and $z is: $largest\n";
+
   
-  echo "Largest number among $x, $y and $z is: $largest";
-}
-
-
 
 ?>
