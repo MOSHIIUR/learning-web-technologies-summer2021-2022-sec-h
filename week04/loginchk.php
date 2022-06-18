109f19ca86e6c592bc1c8124cac5c0c28528e1b2
Moshiur Rahman <sowrav106@gmail.com>
2022-06-18 20:06:40 +0600

    naming validation successful

--- week04/loginchk.php
+++ week04/loginchk.php
@@ -7,19 +7,77 @@ $name = $_POST['name'];
 //print_r($_POST);
 //echo $_POST'name';
 
-$length = strlen($name);
+//word counting
+$wrdCount = str_word_count($name); 
 
 
+//convert the string to array
+$arr = str_split($name);
+//print_r($arr); //print the whole array
+//echo $arr[0]; //just a specific index character
 
-if($name != null && $length >=2 && ctype_alpha($name) && ctype_alpha($name[0]))
+//array size
+$length = sizeof($arr);
+
+$status=true;
+
+if($name != null && $wrdCount >=2)
 {
     
-
+    if(checkLetter($arr[0]))
+    {
+        for($i=1; $i<$length; $i++)
+        {
+         
+         if (checkLetter($arr[$i])|| check_specialChar($arr[$i])) 
+         
+        {
+            $status = true;
+        } 
+        
+        else $status = false;
+        }
+        
+     }
+        
+        
+        
     
-    echo "login Success";
+    
+    else $status = false;
 }
 
-else echo "not success"
+    if($status)
+    {
+    echo "Naming success";
+    }
+    
+    else echo "name is not accepted";
+    
+
+
+    
+//A function to check if a character is a Alphabet or not
+function checkLetter($x)
+{
+    if (($x >= 'a' && $x <= 'z') || ($x >= 'A' && $x <= 'Z')) {
+        return true;
+    } else {
+        return false;
+}
+}
+
+//A function to check if a character is a 'dash' or 'dot'
+function check_specialChar($x)
+{
+    if (($x == '-') || ($x == '.') || ($x == ' ')) {
+        return true;
+    } else {
+        return false;
+}
+}
+
+
 
 
 
