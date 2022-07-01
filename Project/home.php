e3b6bcc7eaccfbb0d11fd2cbf807cbab6a5d32d9
Moshiur Rahman <sowrav106@gmail.com>
2022-07-02 03:10:49 +0600

    home.html deleted, home.php replaced it

--- Project/home.php
+++ Project/home.php
@@ -1,25 +1,36 @@
-<?php
-if(isset($_COOKIE['status']))
-    {
-        echo '
-        
-        <center>
 
-            <h1> Home</h1>
-            <a href="search.html">Search</a>
-            <a href="profile.html">View Profile</a>
-            <a href="addStd.html">Student Profiles</a>
-            <a href="facultyprofile.html">Faculty Profiles</a>
-            <a href="notice.html">Notices</a>
-            <a href="addCourse.html">Courses</a>
-        
-        </center>
-        
-        
-        
-        ';
-       // header('location:header.php');
+<?php
+
+    $username = $_REQUEST['name'];
+    $password = $_REQUEST['password'];
     
+    //set cookies for credentials
+    setcookie('name', $username, time()+356985, '/');
+    setcookie('password', $password, time()+356985, '/');
+
+	if(isset($_COOKIE['name']))
+	{
+        //php end
+        ?> 
+    <center>
+
+        <h1>Wecome Home, <?php echo $_COOKIE['name'];?> </h1>
+        <a href="search.html">Search</a>
+        <a href="userProfile.php">View Profile</a>
+        <a href="addStd.html">Student Profiles</a>
+        <a href="facultyprofile.html">Faculty Profiles</a>
+        <a href="notice.html">Notices</a>
+        <a href="addCourse.html">Courses</a>
+
+    </center>
+
+<?php  //php start
     }
+    
+    else
+    {
+            echo "<h1>nvalid Request to Accessing Home Page</h1>";
+    } 
+
 ?>
 
