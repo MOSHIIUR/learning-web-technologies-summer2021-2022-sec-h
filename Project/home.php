d131592b6dd4a98e15aede2b12e832d9627f4050
Moshiur Rahman <sowrav106@gmail.com>
2022-07-01 11:35:25 +0600

    F#02-[HOME] Draft#01

---
+++ Project/home.php
@@ -0,0 +1,22 @@
+<?php
+if(isset($_COOKIE['status']))
+    {
+        echo '
+        
+        <center>
+
+            <h1> Home</h1>
+            <a href="search.html">Search</a>
+            <a href="profile.html">View Profile</a>
+            <a href="notice.html">Notices</a>
+        
+        </center>
+        
+        
+        
+        ';
+       // header('location:header.php');
+    
+    }
+?>
+
