ba4badd3d84dc192b340abbb4bb82817eb5c4a10
Moshiur Rahman <sowrav106@gmail.com>
2022-07-01 16:36:25 +0600

    F#02-[HOME] Draft#02

--- Project/home.php
+++ Project/home.php
@@ -8,7 +8,10 @@ if(isset($_COOKIE['status']))
             <h1> Home</h1>
             <a href="search.html">Search</a>
             <a href="profile.html">View Profile</a>
+            <a href="addStd.html">Student Profiles</a>
+            <a href="facultyprofile.html">Faculty Profiles</a>
             <a href="notice.html">Notices</a>
+            <a href="addCourse.html">Courses</a>
         
         </center>
         
